2026e12b9db1bc331aa3333b3a064d4e2a5e4511
Ken Klooster <kklooster3910@gmail.com>
2019-08-02 16:42:08 -0400

    commit

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,9 +6,11 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-rails db:purge db:migrate db:seed
+rails db:purge 
+rails db:migrate 
+rails db:seed
 demo_user = User.create!(username: 'Tyler Durden', email: 'fight@club.net', password: 'password123')
-Photo.create!(title: 'first pic' photographer_id: demo_user.id )
-Photo.create!(title: 'second pic' photographer_id: demo_user.id )
-Photo.create!(title: 'third pic' photographer_id: demo_user.id )
-Photo.create!(title: 'fourth pic' photographer_id: demo_user.id )
+Photo.create!(title: 'first pic', photographer_id: demo_user.id )
+Photo.create!(title: 'second pic', photographer_id: demo_user.id )
+Photo.create!(title: 'third pic', photographer_id: demo_user.id )
+Photo.create!(title: 'fourth pic', photographer_id: demo_user.id )
