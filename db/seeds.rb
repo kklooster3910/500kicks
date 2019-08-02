df7a2760d23d61179f53a47d28e36d4794d251f5
Ken Klooster <kklooster3910@gmail.com>
2019-08-02 16:36:31 -0400

    commit trying to get photos seeded into heroku properly

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,5 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+rails db:purge db:migrate db:seed
 demo_user = User.create!(username: 'Tyler Durden', email: 'fight@club.net', password: 'password123')
-# Photo.create!(title; photographer_id: demoUser.id )
+Photo.create!(title: 'first pic' photographer_id: demoUser.id )
+Photo.create!(title: 'second pic' photographer_id: demoUser.id )
+Photo.create!(title: 'third pic' photographer_id: demoUser.id )
+Photo.create!(title: 'fourth pic' photographer_id: demoUser.id )
