eae57b82897c10e82712c324c2a83c3d6bcb9866
Ken Klooster <kklooster3910@gmail.com>
2019-10-01 20:14:16 -0400

    commit webpack for heroku fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,33 @@
 const path = require('path');
 
 module.exports = {
-    context: __dirname,
-    entry: './frontend/500kicks.jsx',
-    output: {
-        path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
-        filename: 'bundle.js'
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '*']
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx?$/,
-                exclude: /(node_modules)/,
-                use: {
-                    loader: 'babel-loader',
-                    query: {
-                        presets: ['@babel/env', '@babel/react']
-                    }
-                },
-            }
-        ]
-    },
-    devtool: 'source-map'
+  context: __dirname,
+  entry: "./frontend/500kicks.jsx",
+  output: {
+    path: path.resolve(__dirname, "app", "assets", "javascripts"),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: [".js", ".jsx", "*"]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules)/,
+        use: {
+          loader: "babel-loader",
+          query: {
+            presets: ["@babel/env", "@babel/react"]
+          }
+        }
+      }
+    ]
+  },
+  devtool: "source-map",
+  plugins: [
+    new webpack.DefinePlugin({
+      "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+    })
+  ]
 };
